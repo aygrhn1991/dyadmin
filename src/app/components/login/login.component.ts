6ab79875f67f6b264bc8f8355c6b5bda01376645
aygrhn1991 <411610370@qq.com>
2018-09-05 15:27:12 +0800

    1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routeInfo: ActivatedRoute,private location:Location) { }
 
   ngOnInit() {
+    console.log(this.location.path());
+    console.log(this.routeInfo.snapshot.paramMap.get('id'));
   }
 
 }
