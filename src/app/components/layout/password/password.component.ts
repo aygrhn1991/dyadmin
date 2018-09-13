ac37d6e7077c4f5bb1a4cf748c5c598c110e67b6
chenyufeng <411610370@qq.com>
2018-09-13 23:51:48 +0800

    1

--- src/app/components/layout/password/password.component.ts
+++ src/app/components/layout/password/password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpserviceService } from '../../../services/httpservice.service';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-password',
@@ -10,9 +12,26 @@ export class PasswordComponent implements OnInit {
   oldpassword: string = '';
   newpassword: string = '';
   confirmpassword: string = '';
-  constructor() { }
+  constructor(private http: HttpserviceService, private message: NzMessageService) { }
 
   ngOnInit() {
+  }
+
+  save(): void {
+    if (this.newpassword != this.confirmpassword) {
+      this.message.create('error', '新密码不一致');
+      return;
+    }
+    this.http.get('/admin/changepassword/' + this.oldpassword + '/' + this.newpassword).subscribe((data: any) => {
+      if (data == true) {
+        this.message.create('success', '操作成功');
+      } else {
+        this.message.create('error', '操作失败');
+      }
+      this.oldpassword = '';
+      this.newpassword = '';
+      this.confirmpassword = '';
+    });
   }
 
 }
