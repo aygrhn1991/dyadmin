5e268a03c2dec347eed6010d27ec4859dccb5741
chenyufeng <411610370@qq.com>
2018-10-10 20:07:17 +0800

    1

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -5,9 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
 
-  // webServer: string = 'http://dy.ljwenyi.com/dy';
-  // fileServer: string = 'http://dy.ljwenyi.com/dyfile/upload';
-  webServer: string = 'http://localhost:4208/dy';
-  fileServer: string = 'http://localhost:4208/dyfile/upload';
+  webServer: string = 'http://dy.ljwenyi.com/dy';
+  fileServer: string = 'http://dy.ljwenyi.com/dyfile/upload';
   constructor() { }
 }
